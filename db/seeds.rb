63aa1b466ca9f92669e4dae843a470bce413afd0
jr1412594 <jr.medina1412@gmail.com>
2020-10-01 13:47:55 -0600

    More complete story

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ dog2 = Dog.create breed: 'Wolf', age: 4, name: 'Kachina', weight: 75, size: 'Lar
 dog3 = Dog.create breed: 'Jackrussel', age: 10, name: 'doby', weight: 10, size: 'Small', life_expectancy: 15, sex: 'Female', price: 100
 dog4 = Dog.create breed: 'Doberman', age: 10, name: 'sky', weight: 65, size: 'Large', life_expectancy: 12, sex: 'Male', price: 300
 dog5 = Dog.create breed: 'Cane Corso', age: 5, name: 'liam', weight: 80, size: 'Medium', life_expectancy: 12, sex: 'Male', price: 400
-dog6 = Dog.create breed: 'Great Dane', age: 5, name: 'Luke', weight: 18, size: 'Small', life_expectancy: 13,sex: 'Male', price: 200
+dog6 = Dog.create breed: 'Great Dane', age: 5, name: 'Luke', weight: 18, size: 'Large', life_expectancy: 13,sex: 'Male', price: 200
 dog7 = Dog.create breed: 'Wolfhound', age: 4, name: 'D_boy', weight: 60, size: 'Large', life_expectancy: 10, sex: 'Male', price: 400
 dog8 = Dog.create breed: 'Neapolitan mastiff', age: 10, name: 'Mike', weight: 80, size: 'Medium', life_expectancy: 10, sex: 'Male', price: 300
 dog9 = Dog.create breed: 'St. Bernard', age: 10, name: 'Jake', weight: 55, size: 'Large', life_expectancy: 10, sex: 'Female', price: 300
@@ -13,6 +13,7 @@ dog10 = Dog.create breed: 'Akita', age: 10, name: 'Olly', weight: 60, size: 'Lar
 dog11 = Dog.create breed: 'Deerhound', age: 10, name: 'Nina', weight: 65, size: 'Large', life_expectancy: 11, sex: 'Female', price: 200
 dog12 = Dog.create breed: 'Bullmastivel', age: 10, name: 'Roxy', weight: 65, size: 'Medium', life_expectancy: 10, sex: 'Male', price: 100
 dog13 = Dog.create breed: 'Alaskan Malamute', age: 10, name: 'Eve', weight: 70, size: 'Large', life_expectancy: 12, sex: 'Male', price: 250
-dog14 = Dog.create breed: 'English Mastive', age: 10, name: 'Thor', weight: 75, size: 'Large', life_expectancy: 12, sex: 'Male', price: 200
+dog14 = Dog.create breed: 'Chihuahua', age: 10, name: 'Thor', weight: 7, size: 'Small', life_expectancy: 20, sex: 'Male', price: 200
 dog15 = Dog.create breed: 'Tibetan Mastive', age: 7, name: 'Lucifer', weight: 145, size: 'Large', life_expectancy: 15, sex: 'Male', price: 500
+
 
