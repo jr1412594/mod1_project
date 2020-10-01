ee7387e5e63ac6244577759bb70b0695876ae5a5
jr1412594 <jr.medina1412@gmail.com>
2020-10-01 17:22:47 -0600

    Single Product Created

--- lib/models/dog.rb
+++ lib/models/dog.rb
@@ -1,7 +1,8 @@
 class Dog < ActiveRecord::Base
-
+    has_many :purchases
+    has_many :products, through: :purchases
     def self.call_by_name(dog_name)
-        puts "BARK BARK BARK BARK!!!!!"
+        # puts "BARK BARK BARK BARK!!!!!"
          all.find do |dog|
             dog.name == dog_name
          end
--- lib/models/product.rb
+++ lib/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ActiveRecord::Base
+has_many :purchases
+has_many :dogs, through: :purchases
+
 end
